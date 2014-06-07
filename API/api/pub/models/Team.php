132c89440303e91eab3cfeb557f5d0ccf3a2caee
nascar246788 <david_vu@alumni.baylor.edu>
2014-06-06 17:52:09 -0700

    Database and API
    Added more to the team model. Still working on it. Also created initial
    import script for us to work with.

--- API/api/pub/models/Team.php
+++ API/api/pub/models/Team.php
@@ -29,7 +29,51 @@
         public function UpdateTeam($id)
         {
             global $db;
-            $query = sprintf("update team set 
+            $query = sprintf("update team set teamName='%s', teamLocationId='%d', teamDescription='%s', teamManaged='%s' where teamId = %d",
+            myEsc($this->teamName),
+            myEsc($this->teamLocationId),
+            myEsc($this->teamDescription),
+            myEsc($this->teamManaged),
+                        			
+			//send back code if successful or not
+			$sql = mysql_query($query, $db);	
+			
+			return $sql;
+        }
+        
+        public function SelectTeamFromId($id)
+        {
+            global $db;
+            if($id)
+            {
+                $query = "select from team where teamId =" .myEsc($id);
+                $sql = mysql_query($sql) > 0)
+                if(mysql_num_rows($sql) > 0)
+                {
+                    $result = array();
+                    while($rlt = mysql_fetch_array($sql, MYSQL_ASSOC))
+                    {
+                        &result[] = $rlt;
+                    }
+                    
+                    foreach($result[0] as $item=>$value(
+                    {
+                        &tTeam->$item = $value;
+                    }
+                    return $tTeam;
+                }
+            }
+            else
+            {
+                //What do we want to do with empty id field?
+                //Error message
+            }
+        }
+        
+        public function SelectTeamFromTeamName($teamName)
+        {
+            global $db;
+            
         }
         
     
