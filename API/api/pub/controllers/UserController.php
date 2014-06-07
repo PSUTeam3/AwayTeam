bc0a5299c99d251e7c597d48cc1bfc6d467799c6
Steve Naimoli <steve@naimolinet.com>
2014-06-07 01:08:10 -0400

    Fixed more formatting 	modified:   API/api/pub/controllers/UserController.php

--- API/api/pub/controllers/UserController.php
+++ API/api/pub/controllers/UserController.php
@@ -1,62 +1,62 @@
 <?php
     //OWNER: S. NAIMOLI
 
-	require_once('/home/awayteam/api/pub/models/User.php');
-	require_once('/home/awayteam/api/pub/apiconfig.php');
+    require_once('/home/awayteam/api/pub/models/User.php');
+    require_once('/home/awayteam/api/pub/apiconfig.php');
 
-	//logic functions
-	class UserController extends User
-	{
-		public function ModifyUser($usrArray)
-		{
-			
-			$tUser = new User;
-			$tUser = $this->arrayToObject($usrArray);
-			$code = $tUser->UpdateUser();
-			
-			//send return code
-	
-			return $code;
-		}
+    //logic functions
+    class UserController extends User
+    {
+        public function ModifyUser($usrArray)
+        {
+            
+            $tUser = new User;
+            $tUser = $this->arrayToObject($usrArray);
+            $code = $tUser->UpdateUser();
+            
+            //send return code
+    
+            return $code;
+        }
 
-		public function CreateUser($usrArray)
-		{
-			$tUser = new User;
+        public function CreateUser($usrArray)
+        {
+            $tUser = new User;
 
-			$tUser = $this->arrayToObject($usrArray);
+            $tUser = $this->arrayToObject($usrArray);
 
-			$newUid = $tUser->InsertUser();
-			//gets new UserID #
+            $newUid = $tUser->InsertUser();
+            //gets new UserID #
 
-			return $newUid;
-		}
+            return $newUid;
+        }
 
-		public function DeleteUser($user)
-		{
-			return $this->DeleteUser($user->userId);
-		}
+        public function DeleteUser($user)
+        {
+            return $this->DeleteUser($user->userId);
+        }
 
-		public function GetUserFromID($userId)
-		{
-			return $this->SelectUserFromID($userId);
-		}
+        public function GetUserFromID($userId)
+        {
+            return $this->SelectUserFromID($userId);
+        }
 
         public function GetUserFromLoginID($loginId)
         {       
             $arr = $this->SelectUserFromLoginID($loginId);
-            return $arr;	
+            return $arr;    
         }
 
-		private function arrayToObject($array)
-		{
-			$tUser = new User;
+        private function arrayToObject($array)
+        {
+            $tUser = new User;
             //convertArray to User Object
             foreach($array as $item=>$value)
             {
-                    $tUser->$item = $value;
+                $tUser->$item = $value;
             }
-			
-			return $tUser;
-		}
-	}
+            
+            return $tUser;
+        }
+    }
 ?>
